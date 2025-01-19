0b6a3c68b9417ad7400bafd35a0a5a5fc53795fd
Aanchallll18 <kumariaachal002@gmail.com>
2025-01-19 13:20:09 +0530

    commit

---
+++ gcd.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int gcd(int n,int m){
+
+}
+int main(){
+    int n,m;
+    cout<<"Enter value of one number: ";
+    cin>>n;
+    cout<<"Enter the value of secound number: ";
+    cin>>m;
+
+    int num=gcd(n,m);
+    cout<<num;
+    return 0;
+}
