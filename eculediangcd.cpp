fd989e5a4d60f1f6255078e63c3f68c70bdd1fd8
Aanchallll18 <kumariaachal002@gmail.com>
2025-01-19 13:57:38 +0530

    commit

---
+++ eculediangcd.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+void gcd(int a,int b){
+    while(a>0 && b>0){
+        if(a>b) a=a%b;
+        else b=b%a;
+    }
+    if(a==0) cout<<b;
+    else cout<<a;
+}
+
+int main(){
+    int a,b;
+    cout<<"enter first number: ";
+    cin>>a;
+    cout<<"enter second number: ";
+    cin>>b;
+
+    gcd(a,b);
+    return 0;
+}
